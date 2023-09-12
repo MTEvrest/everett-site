5cd80ed7130baef2b4267787d77840e114e5532f
Everett White <eadamwhite@gmail.com>
2023-09-12 17:29:47 -0400

    fix: fix website url

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+console.log("here");
 root.render(
   <React.StrictMode>
     <App />
